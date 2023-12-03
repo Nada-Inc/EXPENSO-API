7677eb8eed03bffa504b7fb94dcb868935b1f24c
Vishnu A B <vishnuab1207@gmail.com>
2023-12-03 18:09:05 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,24 @@
+const app = require("./app");
+
+// Handling uncaught Exception
+// process.on("uncaughtException", (err) => {
+//   console.log(`Error: ${err.message}`);
+//   console.log(`shutting down the server for handling uncaught exception`);
+// });
+
+// unhandled promise rejection
+// process.on("unhandledRejection", (err) => {
+//   console.log(`Shutting down the server for ${err.message}`);
+//   console.log(`shutting down the server for unhandle promise rejection`);
+
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
+
+// create server
+const server = app.listen(process.env.PORT, () => {
+  console.log(
+    `Server is running on http://localhost:${process.env.PORT}`
+  );
+});
