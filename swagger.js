ac5357167d9459d27860f86f4aa5b4b4e7b3e2d2
ananduremanan <ananduor@gmail.com>
2023-12-20 09:37:51 +0530

    Swagger Docs Updated

--- swagger.js
+++ swagger.js
@@ -1,6 +1,6 @@
 const swaggerAutogen = require('swagger-autogen')()
 
 const outputFile = './swagger_output.json'
-const endpointsFiles = ['./controller/user.js', './controller/fixedIncome.js', './controller/fixedIncomeData.js']
+const endpointsFiles = ['./app.js']
 
 swaggerAutogen(outputFile, endpointsFiles)
